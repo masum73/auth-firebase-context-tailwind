1f4feb4cecb8caab26de4379881048a722069479
Masum Rahman <masumrahman73@gmail.com>
2023-04-27 23:44:18 +0600

    M60-4 access context value from anywhere inside the app

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbr6jkOq3u3ljFeS1A1gSP--Yue4DxudE",
+  authDomain: "auth-firebase-context-ta-e6080.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-e6080",
+  storageBucket: "auth-firebase-context-ta-e6080.appspot.com",
+  messagingSenderId: "543604217950",
+  appId: "1:543604217950:web:19a57fd2ceb2dd11653ccf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
